e5b58dad919e1ce3fde574cb75f5e4ed09359580
Texas0295 <Texas0295@outlook.com>
2025-04-26 02:50:31 +0800

    get rid of specific tool

--- checkvpn.sh
+++ checkvpn.sh
@@ -1,17 +1,22 @@
-v2raya_status=$(systemctl is-active --quiet v2raya&& echo running)
+#!/bin/bash
 
-if [[ $v2raya_status = "running" ]]
-then
-	connection_stat=$(sudo v2raya --report connection | grep "not running" > /dev/null 2>&1 && echo "disactivated")
-	
-	if [[ $connection_stat = "disactivated" ]]
-	then
-    		stat=' '
-	else
-    		stat=' '
-	fi
+cache_file="/tmp/freedom_flag"
+expire=60
+
+if [[ -f $cache_file && $(($(date +%s) - $(stat -c %Y "$cache_file"))) -lt $expire ]]; then
+    stat=$(cat $cache_file)
 else
-	stat=' '
+    country=$(curl -s --max-time 5 https://ipapi.co/country/)
+    free_net=("HK" "US" "JP" "TW" "DE" "FR" "CA" "GB" "SG")
+
+    stat=' '
+    for code in "${free_net[@]}"; do
+        if [[ "$country" == "$code" ]]; then
+            stat=' '
+            break
+        fi
+    done
+    echo "$stat" > "$cache_file"
 fi
 
 echo -e "$stat"
